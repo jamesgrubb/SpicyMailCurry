8159d170957f5641fe1a37e40eb9effc43f30a56
James Grubb <james@jamesgrubb.co.uk>
2015-11-12 23:10:49 +0000

    Fontstack change

---
+++ grunt/browserSync.js
@@ -0,0 +1,14 @@
+module.exports = {
+  dev: {
+                bsFiles: {
+                    src : [
+                        'css/*.css',
+                        'index.html'
+                    ]
+                },
+                options: {
+                    watchTask: true,
+                    server: './'
+                }
+            }
+}
